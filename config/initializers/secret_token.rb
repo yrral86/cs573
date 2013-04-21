ba5e9b4572056071e94c9638e6c4ecf9c86925ef
Larry Reaves <larry@yrral.net>
2013-04-21 19:11:38 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TuringTest::Application.config.secret_token = '2cb23577a51cc4e25d07d920447adcadee06df89c5a8058a7873908d33ebd1b7dfd32bdbc6a61c255bd434a2ac6e33c27c99505782e2d73b4b2f134ca12f3288'
