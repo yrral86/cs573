5f74ecae729f2a18485048cbfbb2d77f5ee3c085
Larry Reaves <larry@yrral.net>
2013-04-21 20:27:53 -0400

    db session store with custom fields

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-TuringTest::Application.config.session_store :cookie_store, key: '_turing_test_session'
+TuringTest::Application.config.session_store :active_record_store
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
