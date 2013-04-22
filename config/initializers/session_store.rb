7453b890e53c174177c7b374188dc487d5015bfb
Larry Reaves <larry@yrral.net>
2013-04-21 20:50:33 -0400

    go back to cookie sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-TuringTest::Application.config.session_store :active_record_store
+TuringTest::Application.config.session_store :cookie_store, key: '_turing_test_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
