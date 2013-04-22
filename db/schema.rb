a0f3ebaf1a1a17254566e8123b7073af27681c60
Larry Reaves <larry@yrral.net>
2013-04-21 21:35:44 -0400

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130422011645) do
+ActiveRecord::Schema.define(:version => 20130422013509) do
+
+  create_table "chords", :force => true do |t|
+    t.text     "name"
+    t.text     "file"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "test_sessions", :force => true do |t|
     t.text     "user_agent"
