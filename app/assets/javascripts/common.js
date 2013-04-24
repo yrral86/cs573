29ef6707aeaab66a413a633b5132eea6daebe6bf
Larry Reaves <larry@yrral.net>
2013-04-24 16:20:03 -0400

    fix audio playing

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,15 +1,19 @@
 function play_sequence(a,b,c,d,e,f,g,h,i,j) {
     alert([a,b,c,d,e,f,g,h,i,j].join());
-    var audio = new buzz.group(
-        new buzz.sound("/audio/" + a),
-        new buzz.sound("/audio/" + b),
-        new buzz.sound("/audio/" + c),
-        new buzz.sound("/audio/" + d),
-        new buzz.sound("/audio/" + e),
-        new buzz.sound("/audio/" + f),
-        new buzz.sound("/audio/" + g),
-        new buzz.sound("/audio/" + h),
-        new buzz.sound("/audio/" + i),
-        new buzz.sound("/audio/" + j));
-    audio.play();
+    var sounds = []
+    sounds[0] = new buzz.sound("/audio/" + a);
+    sounds[1] = new buzz.sound("/audio/" + b);
+    sounds[2] = new buzz.sound("/audio/" + c);
+    sounds[3] = new buzz.sound("/audio/" + d);
+    sounds[4] = new buzz.sound("/audio/" + e);
+    sounds[5] = new buzz.sound("/audio/" + f);
+    sounds[6] = new buzz.sound("/audio/" + g);
+    sounds[7] = new buzz.sound("/audio/" + h);
+    sounds[8] = new buzz.sound("/audio/" + i);
+    sounds[9] = new buzz.sound("/audio/" + j);
+    for (var i = 0; i < 10; i++) {
+	setTimeout(function(sound) {
+	    sound.play();
+	},i*1500, sounds[i]);
+    }
 }
