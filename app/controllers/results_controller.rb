d23d05591dcd613b0ebc7b1d84950f36e5b9ec83
Larry Reaves <larry@yrral.net>
2013-04-26 16:54:44 -0400

    work on results page

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,4 +1,11 @@
 class ResultsController < ApplicationController
   def index
+    @all_trials = SequenceTrial.completed_trials
+    @human_trials = SequenceTrial.human_trials
+    @correct_human_trials = SequenceTrial.correct_human_trials
+    @incorrect_human_trials = SequenceTrial.incorrect_human_trials
+    @computer_trials = SequenceTrial.computer_trials
+    @correct_computer_trials = SequenceTrial.correct_computer_trials
+    @incorrect_computer_trials = SequenceTrial.incorrect_computer_trials
   end
 end
--- app/models/sequence_trial.rb
+++ app/models/sequence_trial.rb
@@ -2,4 +2,54 @@ class SequenceTrial < ActiveRecord::Base
   attr_accessible :correct, :sequence_id, :test_session_id
   belongs_to :sequence
   belongs_to :test_session
+
+  def self.completed_trials
+    self.where("correct IS NOT NULL")
+  end
+
+  def self.correct_trials
+    self.where(:correct => true)
+  end
+
+  def self.incorrect_trials
+    self.where(:correct => false)
+  end
+
+  def self.computer_trials
+    self.select_computers(self.completed_trials)
+  end
+
+  def self.correct_computer_trials
+    self.select_computers(self.correct_trials)
+  end
+
+  def self.incorrect_computer_trials
+    self.select_computers(self.incorrect_trials)
+  end
+
+  def self.human_trials
+    self.select_humans(self.completed_trials)
+  end
+
+  def self.correct_human_trials
+    self.select_humans(self.correct_trials)
+  end
+
+  def self.incorrect_human_trials
+    self.select_humans(self.incorrect_trials)
+  end
+
+  private
+  def self.select_humans(query)
+    query.select do |t|
+      t.sequence.src.to_sym == :human
+    end
+  end
+
+  def self.select_computers(query)
+    query.select do |t|
+      t.sequence.src.to_sym != :human and
+        t.sequence.src.to_sym != :seed
+    end
+  end
 end
