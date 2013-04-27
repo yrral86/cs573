e87629da5adc2477b044450296eb8ac28f7865e7
Larry Reaves <lareaves@mix.wvu.edu>
2013-04-27 03:20:05 +0000

    add chords in import songs

--- script/import_songs.rb
+++ script/import_songs.rb
@@ -4,9 +4,24 @@ def extract_chords(title)
   open("#{title}").read.gsub(/\s+/, ",").split(",").select{|x| x != ""}
 end
 
+def name_to_file(chord_name)
+  file = chord_name.gsub(/#/, 'sharp')
+  file.gsub!(/b/, 'flat')
+  file.gsub!(/-/, '')
+  file.gsub!(/\//, '')
+  "#{file}.ogg"
+end
+
 chords = []
 files.each do |s|
   chords << extract_chords(s)
+end
+
+unique_chords = chords.flatten.uniq
+
+unique_chords.each do |c|
+  c = Chord.create(:name => c,
+                   :file => name_to_file(c))
 end
 
 chords.each do |s|
