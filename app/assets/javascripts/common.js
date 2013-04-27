a74898bdfe7247cc9a8ae085b1b8dcbd02996d5d
Larry Reaves <larry@yrral.net>
2013-04-26 20:01:47 -0400

    fix double click -> double playback bug

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,18 +1,26 @@
+var playing = false;
+
 function play_sequence(a,b,c,d,e,f,g,h,i,j) {
-    var sounds = []
-    sounds[0] = new buzz.sound("/audio/" + a);
-    sounds[1] = new buzz.sound("/audio/" + b);
-    sounds[2] = new buzz.sound("/audio/" + c);
-    sounds[3] = new buzz.sound("/audio/" + d);
-    sounds[4] = new buzz.sound("/audio/" + e);
-    sounds[5] = new buzz.sound("/audio/" + f);
-    sounds[6] = new buzz.sound("/audio/" + g);
-    sounds[7] = new buzz.sound("/audio/" + h);
-    sounds[8] = new buzz.sound("/audio/" + i);
-    sounds[9] = new buzz.sound("/audio/" + j);
-    for (var i = 0; i < 10; i++) {
-	setTimeout(function(sound) {
-	    sound.play();
-	},i*1500, sounds[i]);
+    if (!playing) {
+	playing = true;
+	var sounds = [];
+	sounds[0] = new buzz.sound("/audio/" + a);
+	sounds[1] = new buzz.sound("/audio/" + b);
+	sounds[2] = new buzz.sound("/audio/" + c);
+	sounds[3] = new buzz.sound("/audio/" + d);
+	sounds[4] = new buzz.sound("/audio/" + e);
+	sounds[5] = new buzz.sound("/audio/" + f);
+	sounds[6] = new buzz.sound("/audio/" + g);
+	sounds[7] = new buzz.sound("/audio/" + h);
+	sounds[8] = new buzz.sound("/audio/" + i);
+	sounds[9] = new buzz.sound("/audio/" + j);
+	for (var i = 0; i < 10; i++) {
+	    setTimeout(function(sound) {
+		sound.play();
+	    },i*1500, sounds[i]);
+	}
+	setTimeout(function() {
+	    playing = false;
+	}, i*1500);
     }
 }
